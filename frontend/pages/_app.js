d59fb9bd9bb13b541ccae0ff03fe208db275f1f3
sahrohit <sahrohit9586@hmail.com>
2022-04-28 11:43:42 +0530

    Updated Color Config

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,12 +1,25 @@
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/provider";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
+
+import Router, { useRouter } from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+import theme from "../config/theme";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+	return (
+		<AnimatePresence exitBeforeEnter>
+			<ChakraProvider theme={theme}>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</AnimatePresence>
+	);
 }
 
 export default MyApp;
